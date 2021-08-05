fbda66831be617ec0e2ab9519e521023a4c3f999
Michael Joseph <michaeljoseph195@gmail.com>
2021-08-05 19:03:47 +0000

    create documentation.md and move documentation from db.py

--- db.py
+++ db.py
@@ -1,48 +1,13 @@
 """
-Functions for handling databases.
-TODO add unit testing
-
-We use 2 tables:
-
-foods 
-Each row in the table represents a separate food item.
-Columns stored in the table are:
-  food_name: 
-      Self explanatory
-  portion_type: 
-      Each food has 1 or more associated portion types (glass, spoon, slice, etc).
-      Each portion type of a particular food is treated as a separate row in the table.
-      but will be grouped together under the common food name when presented to the user.
-      Omitting a portion type is allowed on the user end, this will simply be treated as.
-      it's own row where the portion type is an empty string.
-  includes_foods: 
-      This acts sort of like an ingredients list which allows foods to be supersets
-      of other foods. each row in this list must be a list containing food name, 
-      portion type and number of servings of a food already present in the table.
-      TODO make sure to prevent circular definitions.
-      TODO make sure to take care of cases when food dependancies are deleted later.
-  base_calories: 
-      When a food cannot be constructed purely as a superset of other foods
-      an integer of calories can be added. This is also useful for defining atomized foods.
-
-record 
-Each row in the table represents one consumed food item. 
-Columns stored in the table are:
-  date: 
-      String in the form "dd-mm-yyyy"
-  food_name: 
-      String, corresponding to the same name as in the foods table
-  portion_type:
-      String, corresponding to the same name as in the foods table
-  servings:
-      Integer, number of items consumed
-      TODO increase this number when adding a new row of the same type on the same day, 
-      rather than adding a new row.
+Low-level functions for directly handling the database.
 """
 
 import sqlite3
 from datetime import datetime
 import json
+
+# TODO make sure to prevent circular definitions in the foods database.
+# TODO make sure to take care of cases when food dependancies are deleted later.
 
 
 def create_db_and_tables(db_path) -> None:
