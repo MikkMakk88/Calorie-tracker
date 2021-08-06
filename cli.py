afcdd3122cd460285500caabe0811c5f4aec531a
Michael Joseph <michaeljoseph195@gmail.com>
2021-08-06 17:46:08 +0000

    make cli prettier and more intuitive

--- cli.py
+++ cli.py
@@ -4,40 +4,56 @@ Functions and logic concerning the command line interface.
 import argparse
 
 # define parsers
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(
+                description="A program to keep track of calories consumed each day.")
 subparsers = parser.add_subparsers(
-                    help='command',
-                    dest='subparser_name')
+                help='command',
+                dest='subparser_name')
 
 add_food_parser = subparsers.add_parser(
-                            'add-food',
-                            help="Add food item to database.")
+                'add-food',
+                help="Add food item to database.")
 
 add_entry_parser = subparsers.add_parser(
-                            'add-entry',
-                            help="Add food entry to the record.")
+                'add-entry',
+                help="Add food entry to the record.")
 
 
 
 # add-food arguments
 add_food_parser.add_argument(
-                'food_name',
+                'food-name',
                 help="Name of food to be added to the database.",
                 type=str)
 
 
 # add-entry arguments
 add_entry_parser.add_argument(
-                'food_name',
+                'food-name',
                 help="Name of food to be added to today's record.",
                 type=str)
+
+add_entry_parser.add_argument(
+                '--portion-type',
+                default='',
+                help="Type of portion (e.g. cup, slice, bowl). Defaults to None.",
+                metavar='',
+                type=str)
+
+add_entry_parser.add_argument(
+                '--servings',
+                default=1,
+                help="Number of servings. Defaults to 1.",
+                metavar='',
+                type=int)
 
 add_entry_parser.add_argument(
                 '--date',
                 choices=['today', 'yesterday', 'DD/MM/YYYY'],
                 default='today',
                 help="Optionally specify a specific day the food should be added to. \
-                    Default is today.",
+                        Defaults to today.",
+                metavar='               {today,yesterday,DD/MM/YYYY}',
                 type=str)
 
 
